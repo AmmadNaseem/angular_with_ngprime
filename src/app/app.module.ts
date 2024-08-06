fc4015a9f50e08cdbc5e48c5209f1251e021e186
Aamad Naseem <ammadnaseem878@gmail.com>
2024-08-06 15:04:11 +0500

    setup the angular and ngprime

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// Import PrimeNG modules
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
+
+
+
+
+export const importedModules=[
+    BrowserModule,
+    AppRoutingModule,
+    TableModule,
+    ButtonModule,
+    ToolbarModule,
+    CheckboxModule,
+    InputTextModule,
+  
+
+    
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  imports: [importedModules],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
